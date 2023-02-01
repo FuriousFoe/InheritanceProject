d51676108144aef99c81ec436058e9f14f378e93
FuriousFoe <rohitparkhe.30@gmail.com>
2023-02-01 18:53:42 +0530

    Updated the files

--- My apps/flutter_project/lib/main.dart
+++ My apps/flutter_project/lib/main.dart
@@ -1,11 +1,22 @@
+
 import 'package:flutter/material.dart';
+import 'package:flutter_project/pages/account_detailspage.dart';
 import 'package:flutter_project/pages/cart_page.dart';
+import 'package:flutter_project/pages/facilities_page.dart';
+import 'package:flutter_project/pages/homedetailspage.dart';
 import 'package:flutter_project/pages/homepage.dart';
+import 'package:flutter_project/pages/button_page.dart';
 import 'package:flutter_project/pages/loginpage.dart';
 import 'package:flutter_project/pages/profile_page.dart';
+import 'package:flutter_project/pages/terms_page.dart';
 import 'package:flutter_project/utils/routes.dart';
 import 'package:flutter_project/widgets/themes.dart';
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+   
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp( MaterialApp(home: const MyApp()));
 }
 class MyApp extends StatelessWidget {
@@ -17,17 +28,26 @@ class MyApp extends StatelessWidget {
        theme: MyTheme.lightTheme(context),
        darkTheme: MyTheme.darkTheme(context),
        debugShowCheckedModeBanner: false,
-       initialRoute: MyRoutes.homeRoute ,
+       initialRoute: MyRoutes.loginRoute,
        routes: {
         "/": (context) => LoginPage(),
         MyRoutes.homeRoute: (context) => HomePage(),
         MyRoutes.loginRoute: (context) => LoginPage(),
         MyRoutes.cartRoute: (context) => CartPage(),
         MyRoutes.profileRoute:(context) => Frame(),
+        MyRoutes.accountRoute:(context) => Frametwo(),
+        MyRoutes.facilityRoute:(context) => Facilities(),
+        MyRoutes.buttonRoute:(context) => Home(),
+        MyRoutes.termsRoute:(context) => Terms(),
+        MyRoutes.accountRoute:(context) => Frametwo(),
+        MyRoutes.homeDetailsRoute:(context) => HomePage(),
       },
     );
   }
 }
+
+
+
 
 
 
