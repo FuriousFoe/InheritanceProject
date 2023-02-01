3da641db65a36f7821270743c606d39cb5022ab3
FuriousFoe <rohitparkhe.30@gmail.com>
2023-02-01 18:22:20 +0530

    successfully created new branch

--- My apps/flutter_project/lib/main.dart
+++ My apps/flutter_project/lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
        theme: MyTheme.lightTheme(context),
        darkTheme: MyTheme.darkTheme(context),
        debugShowCheckedModeBanner: false,
-       initialRoute: MyRoutes.homeRoute,
+       initialRoute: MyRoutes.homeRoute ,
        routes: {
         "/": (context) => LoginPage(),
         MyRoutes.homeRoute: (context) => HomePage(),
